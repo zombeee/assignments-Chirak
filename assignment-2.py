82b9530944b0e7cb74a500d6008a032e8b744423
zombeee <zchirak@yandex.ru>
2016-02-06 18:47:41 +0300

    .com

--- assignment-2.py
+++ assignment-2.py
@@ -8,9 +8,11 @@ def my_max(*args):
     """
     This function try to find max, but still can't do it
     """
+
     if len(args) == 1:
-            if isinstance(args, (list, tuple)):
-                return (sorted(args))[-1]
+        for seq in args:
+            if isinstance(seq, (list, tuple)):
+                return (sorted(seq))[-1]
     else:
         for nums in args:
             if isinstance(nums, int):
@@ -24,4 +26,5 @@ my_max((1, 3, -8, 9))
 my_max(1, 3, -8, 9)
 my_max(1)
 my_max()
-my_max(1, 3, -8, 9, True)
+my_max(1, 3, -8, 9, True, 9.8)
+my_max(0.95, 1, 3, -8, 9, True, 9.8)
