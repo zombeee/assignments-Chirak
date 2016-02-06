6f6fd665126d031414388a8567b128cc367625ad
zombeee <zchirak@yandex.ru>
2016-02-06 18:38:25 +0300

    .com

--- assignment-2.py
+++ assignment-2.py
@@ -9,15 +9,14 @@ def my_max(*args):
     This function try to find max, but still can't do it
     """
     if len(args) == 1:
-        for seq in args:
-            if isinstance(seq, (list, tuple)):
+            if isinstance(args, (list, tuple)):
                 return (sorted(seq))[-1]
-        else:
-            for nums in args:
-                if isinstance(nums, int):
-                    return (sorted(args))[-1]
-                else:
-                    raise ValueError("Can't find max, wrong data format")
+    else:
+        for nums in args:
+            if isinstance(nums, int):
+                return (sorted(args))[-1]
+            else:
+                raise ValueError("Can't find max, wrong data format")
 
 
 my_max([1, 3, -8, 9])
