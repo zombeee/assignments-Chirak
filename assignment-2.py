0d7687b0ac2a23e61a52a9f3d135b0858217ca58
zombeee <zchirak@yandex.ru>
2016-02-07 19:02:13 +0300

    .com

--- assignment-2.py
+++ assignment-2.py
@@ -11,12 +11,24 @@ def my_max(*args):
     """
     def sorter(sequence):
         """
-        This function find max in gives sequence of simple numbers
+        This function find max in given sequence of simple numbers
         """
-        for numbers in sequence:
-            if not isinstance(numbers, int):
+        def bubble_sort(a):
+            '''
+            This function sort the list
+            '''
+            for i in reversed(range(len(a))):
+                for j in range(1, i + 1):
+                    if a[j-1] > a[j]:
+                        a[j], a[j-1] = a[j-1], a[j]
+            return a
+
+        listed_seq = list(sequence)
+        for number in listed_seq:
+            if not isinstance(number, int):
                 raise ValueError("Can't find max, wrong data format")
-        return sorted(sequence)[-1]
+        return bubble_sort(listed_seq)[-1]
+
     if not args:
         raise ValueError("Can't find max, no data given")
     if len(args) == 1:
@@ -29,7 +41,7 @@ def my_max(*args):
     return sorter(args)
 
 
-my_max([1, 3, -8, 9])
+my_max([1, 3, -11, -8, 9, 1])
 my_max((1, 3, -8, 9))
 my_max((1, 3, -8, 9.5))
 my_max(1, 3, -8, 9)
@@ -46,12 +58,24 @@ def my_min(*args):
     """
     def sorter(sequence):
         """
-        This function find min in gives sequence of simple numbers
+        This function find max in given sequence of simple numbers
         """
-        for numbers in sequence:
-            if not isinstance(numbers, int):
-                raise ValueError("Can't find min, wrong data format")
-        return sorted(sequence)[0]
+        def bubble_sort(a):
+            '''
+            This function sort the list
+            '''
+            for i in reversed(range(len(a))):
+                for j in range(1, i + 1):
+                    if a[j-1] > a[j]:
+                        a[j], a[j-1] = a[j-1], a[j]
+            return a
+
+        listed_seq = list(sequence)
+        for number in listed_seq:
+            if not isinstance(number, int):
+                raise ValueError("Can't find max, wrong data format")
+            return bubble_sort(listed_seq)[0]
+
     if not args:
         raise ValueError("Can't find min, no data given")
     if len(args) == 1:
@@ -71,5 +95,5 @@ my_min(1, 3, -8, 9)
 my_min(1)
 my_min(1.1)
 my_min()
-my_min(1, 3, -8, 9, 9.8)
+my_min(7, 3, -8, 9, 9.8)
 
