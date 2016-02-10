b2def8db5f678e08b4f5dcc831e6c495fe47941d
zombeee <zchirak@yandex.ru>
2016-02-10 19:21:58 +0300

    Unworking evaluate_string

--- assignment-2_5.py
+++ assignment-2_5.py
@@ -27,35 +27,45 @@ def my_filter(fn, elements, **kwargs):
 my_filter(bool, [1, 0, [], [1, 3, 5]])
 
 
-def evaluate_string(string):
-    calculator_dict = {
+def calculate(numbers, operations):
+    if len(numbers) != len(operations) + 1:
+        raise ValueError("msg")
+    operation_dict = {
         "+": operator.add,
-        "-": operator.sub,
+        "-": operator.sub
     }
+    numbers_iterator = iter(numbers)
+    acc = next(numbers_iterator)
+    for num, oper in zip(numbers_iterator, operations):
+        oper_func = operation_dict.get(oper)
+        if not oper_func:
+            raise ValueError("Operation {} is not supported".format(oper))
+        acc = oper_func(acc, num)
+    return acc
+
+
+def evaluate_string(string):
     list_to_ignor = ["(", ")", " "]
     numbers = []
     operations = []
+    string_for_numbers = ""
     for things in string:
         if things in list_to_ignor:
             continue
         if things == ".":
             raise ValueError("Supported only integer numbers")
-        if not numbers and things == "-":
+        if not numbers and things is "-":
             numbers.append(0)
         if things.isdigit():
-            numbers.append(int(things))
+                string_for_numbers += things
         else:
+            if string_for_numbers:
+                numbers.append(int(string_for_numbers))
+                string_for_numbers = ""
             operations.append(things)
-    numbers_iterator = iter(numbers)
-    acc = next(numbers_iterator)
-    for num, oper in zip(numbers_iterator, operations):
-        oper_funct = calculator_dict.get(oper)
-        if not calculator_dict.get(oper):
-            raise ValueError("Operation {} is not supported".format(oper))
-        acc = oper_funct(acc, num)
-    return acc
+    return calculate(numbers, operations)
 
-evaluate_string(" -1- 5+6 (+6) - 7 ")
+evaluate_string(" 1- 5+66 (+6) + 7 ")
 
 
 
