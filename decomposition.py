1e4fdbaa8c129e041106baf28890594d12ce387b
zombeee <zchirak@yandex.ru>
2016-02-28 13:48:20 +0300

    Decomposition without recursion

--- decomposition.py
+++ decomposition.py
@@ -8,8 +8,6 @@ from __future__ import division, print_function
 This module decompose given number on maximum number of different summands
 """
 
-decomp_result = []
-
 
 def decompose(n):
     """
@@ -18,21 +16,25 @@ def decompose(n):
     :return: Sorted list of maximum number of different
     summands of the given number
     """
-    if n == 4:
-        decomp_result.append(1)
-        decomp_result.append(3)
-        return decomp_result
-    if n == 2:
-        decomp_result.append(2)
-        return decomp_result
-    if n == 1:
-        decomp_result.append(1)
-        return decomp_result
-    if n % 2:
-        decomp_result.append((n//2+1))
-    else:
-        decomp_result.append(n//2)
-    decompose(n//2)
+    decomp_result = []
+    num = n
+    while num:
+        if num == 4:
+            decomp_result.append(1)
+            decomp_result.append(3)
+            return sorted(decomp_result)
+        if num == 2:
+            decomp_result.append(2)
+            return sorted(decomp_result)
+        if num == 1:
+            decomp_result.append(1)
+            return sorted(decomp_result)
+        if num % 2:
+            decomp_result.append(num//2+1)
+            num //= 2
+        else:
+            decomp_result.append(num//2)
+            num //= 2
     return sorted(decomp_result)
 
 
@@ -40,10 +42,9 @@ def main():
     input_number = raw_input("Input: ")
     if not input_number.isdigit():
         raise ValueError("Not integer given")
-    if (input_number < 1) and (input_number > 10**9):
+    if (int(input_number) < 1) or (int(input_number) > 10**9):
         raise ValueError("Given number out of expected range")
-    decompose(int(input_number))
-    print("Output:", *decomp_result)
+    print("Output:", *decompose(int(input_number)))
 
 
 if __name__ == "__main__":
